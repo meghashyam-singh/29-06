2269651967df0590d4408077776adc934fae67b4
megha <megha@gmail.com>
2026-06-29 20:30:03 +0530

    29-06

--- data.tf
+++ data.tf
@@ -37,3 +37,31 @@ data "aws_ssm_parameter" "backend_alb_sg_id" {
 data "aws_ssm_parameter" "frontend_alb_sg_id" {
     name = "${local.common_name}_frontend_alb_sg_id"
 }
+
+data "aws_ami" "ami_id" {
+    owners = ["973714476881"]
+    most_recent = true
+
+    filter {
+        name = "architecture"
+        values = ["x86_64"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    filter {
+        name = "root-device-name"
+        values = ["/dev/sda1"]
+    }
+}
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+    name = "${local.common_name}_private_subnet_ids"
+}
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+    name = "${local.common_name}_public_subnet_ids"
+}
